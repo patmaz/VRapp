3029a173f197f43575e0f5eb97756dd7556f8c5a
Patryk Mazurkiewicz <patrykiewicz@gmail.com>
2017-08-21 21:49:55 +0200

    first commit

---
+++ index.vr.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+  Model,
+  AmbientLight,
+  Image,
+  Box,
+  VideoPano,
+  VrButton,
+} from 'react-vr';
+
+export default class vrapp extends React.Component {
+  state = {
+    display: 'none',
+  };
+
+  clickHandler = () => {
+    this.setState({
+      display: this.state.display === 'none' ? 'block' : 'none',
+    })
+  };
+
+  render() {
+    return (
+      <View>
+        <VideoPano
+          source={asset('360.mp4')}
+          style={{
+            display: this.state.display,
+          }}
+          loop={true}
+          muted={true}
+          poster={asset('360.JPG')}
+        />
+          <AmbientLight intensity={ 2.6 }  />
+          <Model
+            style={{
+            transform: [
+              {translate: [0, 40, -40]},
+              {scale: 0.3 },
+              {rotateY: 20},
+              {rotateX: 20},
+              {rotateZ: 10} ],
+              display: this.state.display === 'none' ? 'block' : 'none',
+            }}
+            source={{obj:asset('moon.obj'), mtl:asset('moon.mtl')}} lit={true}
+          />
+
+          <VrButton
+            onClick={()=>this.clickHandler()}>
+            <Text
+              style={{
+                backgroundColor: '#000000',
+                borderRadius: 0.1,
+                fontSize: 0.2,
+                layoutOrigin: [0.5, 0.5],
+                paddingLeft: 0.2,
+                paddingRight: 0.2,
+                textAlign: 'center',
+                textAlignVertical: 'center',
+                transform: [{translate: [0, 0, -3]}],
+              }}>
+              click me
+            </Text>
+          </VrButton>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('vrapp', () => vrapp);
---
+++ vr/client.js
@@ -0,0 +1,26 @@
+// Auto-generated content.
+// This file contains the boilerplate to set up your React app.
+// If you want to modify your application, start in "index.vr.js"
+import * as SimpleRaycaster from "simple-raycaster";
+
+// Auto-generated content.
+import {VRInstance} from 'react-vr-web';
+
+function init(bundle, parent, options) {
+  const vr = new VRInstance(bundle, 'vrapp', parent, {
+    // Add custom options here
+    raycasters: [
+      SimpleRaycaster // Add SimpleRaycaster to the options
+    ],
+    cursorVisibility: 'visible',
+    ...options,
+  });
+  vr.render = function() {
+    // Any custom behavior you want to perform on each frame goes here
+  };
+  // Begin the animation loop
+  vr.start();
+  return vr;
+}
+
+window.ReactVR = {init};
